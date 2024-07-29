4e4fef5df09d5f0f63e80667a2933e71a45e4321
Mermas-CC <27junio03@gmail.com>
2024-07-29 16:43:37 -0500

    correccion colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,40 +2,51 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-	  screens: {
-		sm: "480px",
-		md: "768px",
-		lg: "976px",
-		xl: "1440px",
-	  },
-	  colors: {
-		black: "#000000",
-		white: "#FFFFFF",
-		gray: "#0D0C22",
-		stone: "#231F20",
-		lime: "#B9FF66",
-		red:"#ef4444",
-		alternativered: "#b91c1c",
-		gris:"#d6d3d1",
-		zinc: {
-		  100: "#F3F3F3",
-		  200: "#F0F0F0",
-		  300: "#D9D9D9",
-		  400: "#D8D8D8",
-		  500: "#898989",
-		  600: "#B0B0B0",
-		  700: "#787878",
-		  800: "#292A32",
-		  900: "#D9D9D9",
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
 		},
-	  },
-	  boxShadow:{
-        'card':'0px 5px 0px 0px #191A23'
-      },
-	  extend: {
-		fontFamily: {
-			SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+		
+		colors: {
+			black: "#2C2C2C",
+			white: "#FFFFFF",
+			gray: {
+				100: "#F5F5F5",
+				200: "#E0E0E0",
+				300: "#B0B0B0",
+				400: "#4D4D4D",
+				500: "#292A32",
+			},
+			yellow: {
+				100: "#FFF8E1",
+				200: "#FFE082",
+				300: "#FFAB00",
+			},
+			red: {
+				100: "#FFCDD2",
+				200: "#EF9A9A",
+				300: "#E57373",
+				400: "#EF5350",
+				500: "#C62828",
+				600: "#8B0000",
+			},
+			zinc: {
+				100: "#2C2C2C",
+				200: "#F5F5F5",
+				300: "#B0B0B0",
+				400: "#4D4D4D",
+				500: "#FFFFFF",
+			},
 		},
-	  },
+		boxShadow: {
+			'card': '0px 5px 0px 0px #292A32',
+		},
+		extend: {
+			fontFamily: {
+				SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+			},
+		},
 	},
-  };
+};
